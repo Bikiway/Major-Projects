b60b1da2f1ba0dd2ac14ed8873d1c7428e5939e2
Bikiway <mariana.95@outlook.pt>
2024-05-29 17:29:46 +0100

    first commit

---
+++ Jogo de memória - Mariana Oliveira/memory_js_en/save_score.php
@@ -0,0 +1,10 @@
+<?php
+  $fp = fopen("memory_highscore.dat", "a");
+  foreach($HTTP_GET_VARS as $var) { 
+    fputs($fp, "$var" . chr(9)); 
+  }
+  fputs($fp, chr(10)); 
+  fclose($fp);
+  
+  include("show_score.php")
+?>
